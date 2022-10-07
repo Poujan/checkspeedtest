5ad9c13ec427e0f09ac4d485dcef8f9b3a51e634
Jagoo Poujan <sanyogjagoo21@gmail.com>
2022-10-07 12:14:28 +0400

    main dart update

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'DartPing flutter demo Demo',
+      title: 'DartPing flutter Demo',
       home: MyHomePage(),
     );
   }
